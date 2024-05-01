eb079488aef1b0be3dd2090f5a1bb0a0ae45044d
yehiahesham2938 <s-yehia.abdelrasol@zewailcity.edu.eg>
2024-05-01 22:58:35 +0300

    adding button for the admin

--- obj/Debug/net8.0/LibraryManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/LibraryManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16f9a50bf5c0b4f57dd4a4352f47ffb0ad64ed59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e29df6896963109ff8756bb8d8c4a72d71556681")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
