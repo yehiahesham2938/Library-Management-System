3f6ec3ecf840b1271552cf7e32478148aaa43d9a
yehiahesham2938 <s-yehia.abdelrasol@zewailcity.edu.eg>
2024-05-19 13:35:13 +0300

    Adding from Books Form to DataBase

--- Pages/Admin.cs
+++ Pages/Admin.cs
@@ -1,32 +1,82 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Data.SqlClient;
-using Microsoft.Data.SqlClient;
-namespace LibraryManagementSystem.Pages;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
-public class AdminModel : PageModel
+namespace LibraryManagementSystem.Pages
 {
-    private readonly ILogger<AdminModel> _logger;
-    
-    public AdminModel(ILogger<AdminModel> logger)
+    public class AdminModel : PageModel
     {
-        _logger = logger;
-    }
+        private readonly string ConnectionString; 
+        private readonly SqlConnection con;
 
-    public void OnGet()
-    {
-    }
-    public IActionResult OnPostBorrowPagePost()
-    {
-        return RedirectToPage("/BorrowPage");
-    }
-    public IActionResult OnPostReturnedBooksPost()
-    {
-        return RedirectToPage("/ReturnedBooks");
-    }
-    public IActionResult OnPostLostBooksPost()
-    {
-        return RedirectToPage("/LostBooks");
+        public AdminModel()
+        {
+            ConnectionString = "Data Source = YEHIA-HESHAM; Initial Catalog = LibraryManagementSystem; Integrated Security = True";
+            con = new SqlConnection(ConnectionString);
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostInsertBookAsync(string title, string isbn, string author, DateTime publishedDate, IFormFile photo)
+        {
+            try
+            {
+                con.Open(); 
+                byte[]? photoBytes = null;
+
+                if (photo != null && photo.Length > 0)
+                {
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        await photo.CopyToAsync(ms);
+                        photoBytes = ms.ToArray();
+                    }
+                }
+
+                string query = "INSERT INTO Books (Title, ISBN, Author, PublishedDate, Photo) VALUES (@Title, @ISBN, @Author, @PublishedDate, @Photo)";
+                SqlCommand command = new SqlCommand(query, con);
+                command.Parameters.AddWithValue("@Title", title);
+                command.Parameters.AddWithValue("@ISBN", isbn);
+                command.Parameters.AddWithValue("@Author", author);
+                command.Parameters.AddWithValue("@PublishedDate", publishedDate);
+                SqlParameter photoParam = new SqlParameter("@Photo", photoBytes != null ? (object)photoBytes : DBNull.Value);
+                command.Parameters.Add(photoParam);
+
+                await command.ExecuteNonQueryAsync();
+
+                return RedirectToPage("/Admin");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return Page();
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        public IActionResult OnPostBorrowPagePost()
+        {
+            return RedirectToPage("/BorrowPage");
+        }
+
+        public IActionResult OnPostReturnedBooksPost()
+        {
+            return RedirectToPage("/ReturnedBooks");
+        }
+
+        public IActionResult OnPostLostBooksPost()
+        {
+            return RedirectToPage("/LostBooks");
+        }
     }
 }
