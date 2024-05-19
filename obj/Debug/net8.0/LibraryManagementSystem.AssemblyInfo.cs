933523a026d3a7e6c74ea56e5b80fd5a1a6c46c2
yehiahesham2938 <s-yehia.abdelrasol@zewailcity.edu.eg>
2024-05-19 16:06:59 +0300

    Date added to all admin pages

--- obj/Debug/net8.0/LibraryManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/LibraryManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f6ec3ecf840b1271552cf7e32478148aaa43d9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+783ff78c8a1a32f34aceb24e0cbfe0c9a2a03bed")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
