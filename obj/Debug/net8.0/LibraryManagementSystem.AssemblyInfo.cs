8d99870ad0c89b74eed198b466697b7ae5f9b519
yehiahesham2938 <s-yehia.abdelrasol@zewailcity.edu.eg>
2024-05-19 20:40:52 +0300

    Added some files and css codes

--- obj/Debug/net8.0/LibraryManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/LibraryManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89e0d2af5cbed5d25149fe70e8055bb062720cb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7137f81b360251813344c7328bcf2f7b53d10eb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
