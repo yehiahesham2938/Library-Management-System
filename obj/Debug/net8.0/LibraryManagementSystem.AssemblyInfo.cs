225828020c93efa02b83b7bd02025ab5f7865490
yehiahesham2938 <s-yehia.abdelrasol@zewailcity.edu.eg>
2024-05-20 13:07:15 +0300

    Admin Page Done

--- obj/Debug/net8.0/LibraryManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/LibraryManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8878b09094bc5b50625aa5fb2c6e45e74186565")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57e6460d375406fcb082990f5b8e61f0c14d7420")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
